dfddf690c5fca00a780126e2dcf49d4fdaddcabf
raincole <raincolee@gmail.com>
2013-09-08 07:46:55 +0800

    Fix a small bug

--- r.lua
+++ r.lua
@@ -105,8 +105,7 @@ R.metadatas = {
 				R.images.sticks[1]:getWidth() / 2, R.images.sticks[1]:getHeight(),
 				R.images.sticks[1]:getWidth(), R.images.sticks[1]:getHeight()
 			),
-			glow = newQuad(R.images.sticks[1]:getWidth() / 2, 0, 
-			fired = newQuad(R.images.sticks[1]:getWidth() / 2, 0,
+			glow = newQuad(R.images.sticks[1]:getWidth() / 2, 0,
 				R.images.sticks[1]:getWidth() / 2, R.images.sticks[1]:getHeight(),
 				R.images.sticks[1]:getWidth(), R.images.sticks[1]:getHeight()
 			),
